05146efd846968ed22961db9ec595e638cdc92b9
Jack Huang <jack.huang78@gmail.com>
2017-03-07 00:44:50 -0800

    include subject

--- run_analysis.R
+++ run_analysis.R
@@ -10,12 +10,15 @@ y.train = read.table('Y_train.txt')
 y.test = read.table('Y_test.txt')
 y = rbind(y.train, y.test)
 
+# improt and merge subject
+subject.train = read.table('subject_train.txt')
+subject.test = read.table('subject_test.txt')
+subject = rbind(subject.train, subject.test)
+
 label = read.table('activity_labels.txt')
 y.labeled = merge(y, label, by.x='V1', by.y='V1', sort=F)
 
-data.all = data.frame(activity=y.labeled$V2, mean=apply(x, 1, mean), sd=apply(x, 1, sd))
+data.all = data.frame(activity=y.labeled$V2, subject=subject$V1, mean=apply(x, 1, mean), sd=apply(x, 1, sd))
 
-data.grouped = summarize(group_by(data.all, activity), mean=mean(mean), sd=mean(sd))
-
-write.table(data.all, 'data_all.txt', row.names=F)
-write.table(data.grouped, 'data_grouped.txt', row.names=F)
+data.average = summarize(group_by(data.all, activity, subject), mean=mean(mean), sd=mean(sd))
+write.table(data.average, 'data_average.txt', row.names=F)
