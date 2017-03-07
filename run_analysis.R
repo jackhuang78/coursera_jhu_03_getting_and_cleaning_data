5e11880d9907a2529d5c33b3d2fed73302e17101
Jack Huang <jack.huang78@gmail.com>
2017-03-07 01:30:33 -0800

    fixed to include all other mean/std

--- run_analysis.R
+++ run_analysis.R
@@ -1,25 +1,43 @@
-library(dplyr)
-
+#
+# Step 1: Merges the training and the test sets to create one data set.
+#
 # import and merge X
-x.train = read.table('X_train.txt')
-x.test = read.table('X_test.txt')
+x.train = read.table('UCI HAR Dataset/train/X_train.txt')
+x.test = read.table('UCI HAR Dataset/test/X_test.txt')
 x = rbind(x.train, x.test)
 
 # import and merge Y
-y.train = read.table('Y_train.txt')
-y.test = read.table('Y_test.txt')
+y.train = read.table('UCI HAR Dataset/train/Y_train.txt')
+y.test = read.table('UCI HAR Dataset/test/Y_test.txt')
 y = rbind(y.train, y.test)
 
 # improt and merge subject
-subject.train = read.table('subject_train.txt')
-subject.test = read.table('subject_test.txt')
+subject.train = read.table('UCI HAR Dataset/train/subject_train.txt')
+subject.test = read.table('UCI HAR Dataset/test/subject_test.txt')
 subject = rbind(subject.train, subject.test)
 
-label = read.table('activity_labels.txt')
+#
+# Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+#
+features = read.table('UCI HAR Dataset/features.txt')
+names(x) = features[,2]
+x.extract = x[,grep('([Mm][Ee][Aa][Nn]|[Ss][Tt][Dd])', features[,2])]
+
+#
+# Step 3: Uses descriptive activity names to name the activities in the data set
+#
+label = read.table('UCI HAR Dataset/activity_labels.txt')
 y.labeled = merge(y, label, by.x='V1', by.y='V1', sort=F)
 
-data.all = data.frame(activity=y.labeled$V2, subject=subject$V1, mean=apply(x, 1, mean), sd=apply(x, 1, sd))
-write.table(data.all, 'data.all', row.names=F)
+#
+# Step 4: Appropriately labels the data set with descriptive variable names.
+#
+combined = cbind(y.labeled$V2, subject, x.extract)
+names(combined) = c('activity', 'subject', names(x.extract))
 
-data.average = summarize(group_by(data.all, activity, subject), mean=mean(mean), sd=mean(sd))
-write.table(data.average, 'data_average.txt', row.names=F)
+#
+# Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#
+library(dplyr)
+combined_average = summarize_each(group_by(combined, activity, subject), funs(mean))
+write.table(combined_average, 'combined_average.txt', row.names=F)
